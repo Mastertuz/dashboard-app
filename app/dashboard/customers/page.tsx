f412c66ab7e008f3f9f2d9132a71f5a8688a2eca
Mastertuz <futureprogrammer2301@gmail.com>
2025-04-06 21:39:29 +0300

    finished chapter 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Dashboard customers',
+};
 export default function Page() {
     return <p>Customers Page</p>;
   }
